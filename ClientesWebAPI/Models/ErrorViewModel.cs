b947cd7811d17dac83c8ab5848cc84e02f379484
Alexandre Valle <a.rvalle1@hotmail.com>
2021-01-08 20:52:52 -0300

    Projeto WebAPI
    Primeiro commit do projeto WebAPI

---
+++ ClientesWebAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClientesWebAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
