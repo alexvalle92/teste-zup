183c1b4f1ef131d6ad754317e3c0bc2d343e8df0
Alexandre Valle <a.rvalle1@hotmail.com>
2021-01-09 01:25:32 -0300

    Criação do projeto WebApp
    Criação do projeto WebApp para execução do frontend

---
+++ WebApp/Controllers/ClientesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class ClientesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Gerenciar");
+        }
+
+        public IActionResult Gerenciar()
+        {
+            return View();
+        }
+
+        public IActionResult Adicionar()
+        {
+            return View("AdicionarEditar");
+        }
+
+        public IActionResult Editar()
+        {
+            return View("AdicionarEditar");
+        }
+    }
+}
